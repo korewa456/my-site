f27fa98cc481fc4fbfd6074cd1157a0e34005277
Alan <hungjen0816@gmail.com>
2023-04-14 01:33:35 -0700

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import myData from "./product.json";
 
 function App() {
-  const number = [1, 2, 3, 10];
+  const number = [1, 2, 3, 10, 15];
   console.log(myData);
   return (
     <div className="App">
